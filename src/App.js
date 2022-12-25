48d560a88f9604cf6adfc9f0e4178bbcf2c3c2c2
Franck Asket <32412954+asketsystem@users.noreply.github.com>
2022-12-25 02:21:04 +0000

    React Hook Form Structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        {/** Form Column */}
+        <div className="column">
+          <div className="left-half">
+            <h1>Contact Us</h1>
+            <p>We're open for any suggestion or just to have a chat.</p>
+
+            <form></form>
+          </div>
+        </div>
+        {/** Image Column */}
+        <div className="image"></div>
+      </div>
     </div>
   );
 }
